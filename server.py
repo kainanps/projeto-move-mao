c3ce1f5390cff8319dec8f5a4bd34bc87a42de2a
karolinypaiva <137011546+karolinypaiva@users.noreply.github.com>
2026-01-11 14:41:29 -0300

    ajustes

--- server.py
+++ server.py
@@ -44,7 +44,7 @@ def enviar_para_socket(payload):
 
 @app.route('/frame', methods=['POST'])
 def process_frame():
-    print("📨 Request recebido!") # <- Aqui
+    # print("📨 Request recebido!") # <- Aqui
     try:
         data = request.json
         image_data = data.get('image')
